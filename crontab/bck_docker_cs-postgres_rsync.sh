0645266dbc11c28faa3a5b34abe36b17f9f66346
Mikael PANN <mikael.pann@stimio.fr>
2020-12-01 10:59:53 +0100

    [JIRA STIMCLOUD-336] Update backup

--- crontab/bck_docker_cs-postgres_rsync.sh
+++ crontab/bck_docker_cs-postgres_rsync.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-
 if [ "$(docker ps -q -f name=cs-postgres)" ]; then
-
-
 	echo "Backup is running"
-
-	# docker container exec -it cs-postgres /bin/bash
-	# docker container exec -it cs-postgres su
-
 	# Backup Docker DB
 	docker container exec cs-postgres su postgres -c "pg_dump -Fc chirpstack_as > /var/lib/postgresql/data/chirpstack_as.dump"
 	docker container exec cs-postgres su postgres -c "pg_dump -Fc chirpstack_ns > /var/lib/postgresql/data/chirpstack_ns.dump"
+	# Restore db
 	# pg_restore -d chirpstack.as /data/chirpstack.as.dump
-
 fi
 
-
-
-
-
 # Rolling archiving
-
 PathOri="/mnt/VM/docker/chirpstack/postgresqldata/"
 PathBackup="/mnt/BackupContainer/"
 Container="chirpstack"
@@ -36,15 +24,15 @@ fi
 
 # Copy postgres dump DB
 DATE=$(date '+%Y-%m-%d_%Hh_%Mm_%Ss')
-EndFile=$(date '+%Y-%m-%d' --date="30 days ago" )
+HIST="30"
+
+EndFile=$(date '+%Y-%m-%d' --date="$HIST days ago" )
 
 DATEDir=$(date '+%Y-%m')
-DATE_REP_del=$(date '+%Y-%m' --date="30 days ago" )
+DATE_REP_del=$(date '+%Y-%m' --date="${HIS}" )
 
 DATEDay=$(date '+%d')
-DATE_FILE_del=$(date '+%d' --date="30 days ago" )
-
-echo DATE ${DATEDay}
+DATE_FILE_del=$(date '+%d' --date="${HIS}" )
 
 if [ ! -f ${PathOri}${dbnameCheck} ]; then
 	SIZE_SRC="0"
@@ -60,13 +48,8 @@ else
 	SIZE_DST=$(ls -l --sort=t $PathBackup$Container/${DATEDir}/${DATEDay}/ |grep $dbname |head -n1 |awk -F " " '{print $5}')
 fi
 
-
-
-echo "-->SRC $SIZE_SRC<--";
-echo "-->DEST $SIZE_DST<--";
-
 if [ "$SIZE_DST" != "$SIZE_SRC" ]; then
-        # thingsboard
+    # thingsboard
 	apt-get update 
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' rsync |grep "install ok installed")
 	echo Checking for somelib: $PKG_OK
@@ -83,15 +66,10 @@ if [ "$SIZE_DST" != "$SIZE_SRC" ]; then
 	if [ -d "${PathBackup}${Container}/${DATE_REP_del}/${DATE_FILE_del}/" ]; then
 		rm ${PathBackup}${Container}/${DATE_REP_del}/${DATE_FILE_del}/thingsboard_${EndFile}*
 		rmdir ${PathBackup}${Container}/${DATE_REP_del}/${DATE_FILE_del}
-	else 
-		echo "Directory  ${PathBackup}${Container}/${DATE_REP_del}/${DATE_FILE_del}/ not exist for cleaning"
 	fi
-
 
 	rsync  ${PathOri}/${dbname}*.dump  ${PathBackup}${Container}/${DATEDir}/${DATEDay}/${dbname}${DATE}
 
-	echo "Rsync done "
-	echo 	
 
 else 
 	echo "Same file"
